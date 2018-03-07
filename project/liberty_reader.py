aa2f14529f221dbeb5da8d3ec49a7b83348c11e4
Platser <platser@gmail.com>
2018-03-07 11:43:41 +0300

    Dev

--- project/liberty_reader.py
+++ project/liberty_reader.py
@@ -2,26 +2,39 @@
 
 
 #import re
+import logging
+import argparse
+import os
+
 import projlib.liberty
 
 def lb():
     print("\n%s\n" % ( '*'*80 ) )
 
+def main():
+    parser=argparse.ArgumentParser()
+    
+    parser.add_argument("lib_file",help="Liberty file name")
+    parser.add_argument("-debug", help="More detailed logging", action="store_true")
+    
+    args=parser.parse_args()
 
-lib = projlib.liberty.liberty('example.lib')
-#lib.out()
-lib.recursive_parse()
+    if args.debug:
+        logging.basicConfig(filename='liberty.log',filemode='w',level=logging.DEBUG)
+    else:
+        logging.basicConfig(filename='liberty.log',filemode='w',level=logging.INFO)
+
+    lib = projlib.liberty.liberty(os.path.abspath('example.lib'))
+    lib.recursive_parse()
+
+    #lb()
+
+    lib.print_lib()
+    
+
+if __name__ == "__main__":
+    main()
 
 
-#m=re.match('"+','""xxx"""')
-#print(m)
 
-lb()
 
-print(lib.root.keyword)
-for el in lib.root.child_elements:
-    print(el.keyword)
-
-lb()
-
-lib.print_lib()
--- project/projlib/liberty.py
+++ project/projlib/liberty.py
@@ -1,4 +1,5 @@
 import re
+import logging
 
 class liberty_array:
     pass
@@ -6,9 +7,7 @@ class liberty_array:
 
 
 class liberty_attribute:
-    name=""
-    value=""
-    
+
     def __init__(
             self,
             name=None,
@@ -24,47 +23,62 @@ class liberty_attribute:
 
 
 class liberty_element:
-    child_elements=[]
-    attributes=[]
-    arrays=[]
-    keyword=""
-    name=""
-    level=0
+    #child_elements=[]
+    #attributes=[]
+    #arrays=[]
+    #keyword=""
+    #name=""
+    #level=0
     
     def __init__(
             self,
             keyword=None,
             name="",
             level=0):
+        self.child_elements=[]
+        self.attributes=[]
+        self.arrays=[]
         self.keyword=keyword
-        if self.keyword is not None:
-            #print(self.keyword)
-            pass
         self.level=level
         self.name=name
+        
     
     def add_child_element(
             self,
             element):
+        #logging.debug("Adding child element %s ( %s ) to the element %s ( %s )" % (element.keyword, element.name,self.keyword,self.name))
         self.child_elements.append(element)
+        
+    def add_attribute(
+            self,
+            attribute_string):
+        m = re.match('^\s*([a-zA-Z0-9_]+)\s*:\s*([a-zA-Z0-9_”"]+)\s*;\s*$',attribute_string);
+        if m:
+            name=m.group(1)
+            value=m.group(2)
+            new_attribute=liberty_attribute(name=name,value=value)
+            self.attributes.append(new_attribute)
+            return(True)
+        else:
+            return(False)
 
 
 
 class liberty:
-    root=None
-    pos=0
 
     def __init__(
             self,
             filename=None):
-        #print("Hellow World from Liberty class!")
         self.raw=None
         if filename is not None:
             self.read_from_file(filename)
+        self.root=None
+        pos=0
         
     def read_from_file(
             self,
             filename):
+        logging.info("Reading "+ filename)
         with open(filename, 'r') as fh:
             self.raw=fh.read().replace('\n','').replace(' ', '')
             self.raw=re.sub('/\*.*\*/','',self.raw)
@@ -84,9 +98,7 @@ class liberty:
         while pos <= end:
             buffer_end=pos
             if self.raw[pos] == ";":
-                #print(self.raw[buffer_start:pos+1])
-                if self.is_attribute(self.raw[buffer_start:pos+1]):
-                    print("%s%s" % ( '    '*(current_element.level+1), self.raw[buffer_start:pos+1] ) )
+                current_element.add_attribute(self.raw[buffer_start:pos+1])
 
                 # is_array to be here
                     
@@ -94,21 +106,16 @@ class liberty:
                 pos+=1
                 continue
             if self.raw[pos] == "{":
-                #print(self.raw[buffer_start:pos+1])
                 m=re.match('^\s*(\w+)\((\w*)\)',self.raw[buffer_start:pos+1])
                 keyword=m.group(1)
                 name=m.group(2)
                 if current_element is None:
                     self.root=liberty_element(keyword=keyword,name=name)
-                    print("<%s>" % keyword)
                     pos=self.recursive_parse(start=pos+1,current_element=self.root)
-                    print("</%s>" % keyword)
                     buffer_start=pos
                 else:
                     new_element=liberty_element(keyword=keyword,name=name,level=current_element.level+1)
-                    print("%s<%s> %d" % ( '    '*new_element.level, keyword, pos))
                     pos=self.recursive_parse(start=pos+1,current_element=new_element)
-                    print("%s</%s> %d" % ( '    '*new_element.level, keyword, pos))
                     buffer_start=pos
                     current_element.add_child_element(new_element)
                 continue
@@ -119,28 +126,25 @@ class liberty:
     def recursive_print(
             self,
             element):
-        print("element: %s" % element)
-        print("element.child_elements: %s" % element.child_elements )
         print("%s%s ( %s ) {" % ( '    '*element.level, element.keyword, element.name) )
+        for attr in element.attributes:
+            print("%s%s : %s ;" % ('    '*(element.level+1), attr.name, attr.value) )
         for child_element in element.child_elements:
-            print(child_element)
             self.recursive_print(child_element)
-        print("%s}" % '    '*element.level )
+        print("%s}" % ('    '*element.level) )
         
     
     def print_lib(self):
         self.recursive_print(self.root)
         
     
-    def is_attribute(
-            self,
-            string):
-        #print(string)
-        m = re.match('^\s*[a-zA-Z0-9_]+\s*:\s*[a-zA-Z0-9_”"]+\s*;\s*$',string);
-        if m:
-            #print('+')
-            return(True)
-        return(False)
+    # def is_attribute(
+    #         self,
+    #         string):
+    #     m = re.match('^\s*[a-zA-Z0-9_]+\s*:\s*[a-zA-Z0-9_”"]+\s*;\s*$',string);
+    #     if m:
+    #         return(True)
+    #     return(False)
 
     def out(self):
         print(self.raw)
